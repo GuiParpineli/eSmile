87ac9f481097b5668cf4994790d751373d1ed950
GuiParpineli <guiparpineli@hotmail.com>
2022-09-11 11:02:17 -0300

    patienDTO ok

--- src/main/java/com/esmile/appEsmile/controller/PatientController.java
+++ src/main/java/com/esmile/appEsmile/controller/PatientController.java
@@ -1,44 +1,72 @@
 package com.esmile.appEsmile.controller;
 
+import com.esmile.appEsmile.dto.PatientDTO;
 import com.esmile.appEsmile.entity.Patient;
 import com.esmile.appEsmile.service.impl.PatientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/paciente")
 public class PatientController {
 
     @Autowired
-    PatientService patientService;
+    PatientService service;
 
     @PostMapping
     public Patient savePatient(@RequestBody Patient patient) {
 //        patient.setAddress(address);
-        return patientService.save(patient);
+        return service.save(patient);
     }
 
     @GetMapping
-    public Optional<Patient> get(@RequestParam("id") Long id) {
-        return patientService.get(id);
+    public ResponseEntity get(@RequestParam("id") Long id) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Optional<Patient> patientOptional = service.get(id);
+        if (patientOptional.isEmpty()) {
+            return new ResponseEntity("Nenhum paciente encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        PatientDTO patientDTO = mapper.convertValue(patientOptional, PatientDTO.class);
+
+        return new ResponseEntity(patientDTO, HttpStatus.OK);
     }
 
     @GetMapping("/todos")
-    public List<Patient> getAll() {
-        return patientService.getAll();
+    public ResponseEntity getAll() {
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<PatientDTO> patientDTOS = new ArrayList<>();
+
+        List<Patient> patients = service.getAll();
+
+        if (patients.isEmpty()) {
+            return new ResponseEntity("Nenhum paciente cadastrado", HttpStatus.NOT_FOUND);
+        }
+
+        for (Patient p : patients) {
+        patientDTOS.add(mapper.convertValue(p, PatientDTO.class));
+        }
+
+        return new ResponseEntity(patientDTOS, HttpStatus.OK);
+
     }
 
     @PutMapping
     public void update(@RequestBody Patient patient) {
-        patientService.update(patient);
+        service.update(patient);
     }
 
     @DeleteMapping
     public void excluir(@RequestBody Patient patient) {
-        patientService.delete(patient);
+        service.delete(patient);
     }
 
 }
