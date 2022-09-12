73d1391aaa1e28ca3fe92ad54f26f8f722e14f04
FilipeFariaas <filipefarias117@gmail.com>
2022-09-12 20:28:36 -0300

    refactor: getAll dentist to DTO

--- src/main/java/com/esmile/appEsmile/controller/DentistController.java
+++ src/main/java/com/esmile/appEsmile/controller/DentistController.java
@@ -1,13 +1,17 @@
 package com.esmile.appEsmile.controller;
 
 import com.esmile.appEsmile.dto.DentistDTO;
+import com.esmile.appEsmile.dto.PatientDTO;
 import com.esmile.appEsmile.entity.Dentist;
 import com.esmile.appEsmile.service.impl.DentistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,8 +41,22 @@ public class DentistController {
     }
 
     @GetMapping("/todos")
-    public List<Dentist> getAll() {
-        return service.getAll();
+    public ResponseEntity getAll() {
+        ObjectMapper mapper = new ObjectError();
+
+        List<DentistDTO> dentistDTOS = new ArrayList<>();
+
+        List<Dentist> dentists = service.getAll();
+
+        if(dentists.isEmpty()) {
+            return new ResponseEntity("Nenhum dentista encontratdo", HttpStatus.NOT_FOUND);
+        }
+
+        for(Dentist d: dentists) {
+            dentistDTOS.add(mapper.convertValue(d, DentistDTO.class))
+        }
+
+        return new ResponseEntity(dentistDTOS, HttpStatus.OK);
     }
 
     @PutMapping
