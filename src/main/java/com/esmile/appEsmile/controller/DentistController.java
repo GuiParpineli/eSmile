89b3c076d017929409097c6027a1b5f8b80eb303
FilipeFariaas <filipefarias117@gmail.com>
2022-09-12 20:16:33 -0300

    refactor: get dentist to DTO

--- src/main/java/com/esmile/appEsmile/controller/DentistController.java
+++ src/main/java/com/esmile/appEsmile/controller/DentistController.java
@@ -1,8 +1,12 @@
 package com.esmile.appEsmile.controller;
 
+import com.esmile.appEsmile.dto.DentistDTO;
 import com.esmile.appEsmile.entity.Dentist;
 import com.esmile.appEsmile.service.impl.DentistService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -11,31 +15,40 @@ import java.util.Optional;
 @RequestMapping("/dentista")
 public class DentistController {
     @Autowired
-    DentistService dentistService;
+    DentistService service;
 
     @PostMapping
     public Dentist save(@RequestBody Dentist dentist) {
-        return dentistService.save(dentist);
+        return service.save(dentist);
     }
 
     @GetMapping
-    public Optional<Dentist> get(@RequestParam("id") Long id) {
-        return dentistService.get(id);
+    public ResponseEntity get(@RequestParam("id") Long id) {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        Optional<Dentist> dentistOptional = service.get(id);
+        if(dentistOptional.isEmpty()) {
+            return new ResponseEntity("Nenhum dentista encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        DentistDTO dentistDTO = mapper.convertValue(dentistOptional, DentistDTO.class);
+
+        return new ResponseEntity(dentistDTO, HttpStatus.OK);
     }
 
     @GetMapping("/todos")
     public List<Dentist> getAll() {
-        return dentistService.getAll();
+        return service.getAll();
     }
 
     @PutMapping
     public void update(@RequestBody Dentist dentist) {
-        dentistService.update(dentist);
+        service.update(dentist);
     }
 
     @DeleteMapping
     public void delete(@RequestBody Dentist dentist) {
-        dentistService.delete(dentist);
+        service.delete(dentist);
     }
 }
 
