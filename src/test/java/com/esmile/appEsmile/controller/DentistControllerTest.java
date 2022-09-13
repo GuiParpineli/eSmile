5715779f594783d69fb63fda47f1e71ff5350d90
FilipeFariaas <filipefarias117@gmail.com>
2022-09-13 20:55:57 -0300

    add dentistControllerTest class

---
+++ src/test/java/com/esmile/appEsmile/controller/DentistControllerTest.java
@@ -0,0 +1,36 @@
+package com.esmile.appEsmile.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(DentistController.class)
+class DentistControllerTest {
+
+    private MockMvc mvc;
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
