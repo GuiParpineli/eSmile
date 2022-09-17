d05fe01d3e0a65cc382707add0bbc79cc62723d4
FilipeFariaas <filipefarias117@gmail.com>
2022-09-17 19:38:52 -0300

    add deleteDentist success test

--- src/test/java/com/esmile/appEsmile/controller/DentistControllerTest.java
+++ src/test/java/com/esmile/appEsmile/controller/DentistControllerTest.java
@@ -74,7 +74,7 @@ class DentistControllerTest {
 
     @Test
     @DisplayName("Deve Retornar OK - Salvar dentista")
-    void save() {
+    void saveDentist_success() {
 //        Dentist dentist = new Dentist(
 //                1L,
 //                "Maiara",
@@ -109,7 +109,7 @@ class DentistControllerTest {
 
     @Test
     @DisplayName("Deve Retornar OK - Buscar dentista")
-    void get() {
+    void getDentist_success() {
         when(service.get(RECORD_01.getId()))
                 .thenReturn(Optional.of(RECORD_01));
 
@@ -139,7 +139,7 @@ class DentistControllerTest {
 
     @Test
     @DisplayName("Deve Retornar OK - Buscar todos dentistas")
-    void getAll() {
+    void getAllDentists_success() {
         List<Dentist> dentists = new ArrayList<>(Arrays.asList(RECORD_01, RECORD_02, RECORD_03));
 
         when(service.getAll())
@@ -158,10 +158,49 @@ class DentistControllerTest {
     }
 
     @Test
-    void update() {
+    @DisplayName("Deve Retornar OK - Atualizar dentista")
+    void updateDentist_success() {
+        Dentist updatedDentist = Dentist.builder()
+                .id(1L)
+                .name("Filipe")
+                .lastname("Farias")
+                .cro("357984")
+                .build();
+
+        when(service.get(RECORD_01.getId()))
+                .thenReturn(Optional.of(RECORD_01));
+        when(service.save(updatedDentist))
+                .thenReturn(updatedDentist);
+
+        try {
+            MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                    .post("/dentista")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(this.mapper.writeValueAsString(updatedDentist));
+
+            mockMvc.perform(mockRequest)
+                    .andExpect(status().isOk());
+//                    .andExpect(jsonPath("$", notNullValue()))
+//                    .andExpect(jsonPath("$.name", is("Filipe")));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
-    void delete() {
+    @DisplayName("Deve Retornar OK - Deletar dentista")
+    void deleteDentist_success() {
+        when(service.get(RECORD_02.getId()))
+                .thenReturn(Optional.of(RECORD_02));
+
+        try {
+            mockMvc.perform(MockMvcRequestBuilders
+                    .delete("/dentista?id=2")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
