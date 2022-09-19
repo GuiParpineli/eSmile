7746709989f753b00de46e012e2abb9bdfb49c4e
GuiParpineli <guiparpineli@hotmail.com>
2022-09-19 19:25:10 -0300

    fix securityconf

--- src/main/java/com/esmile/appEsmile/login/SecurityConfiguration.java
+++ src/main/java/com/esmile/appEsmile/login/SecurityConfiguration.java
@@ -63,9 +63,10 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
